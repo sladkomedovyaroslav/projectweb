124bfcbbc96da1845c973980f6f75be0528f7503
sladkomedovyaroslav <sladkomedov.yaroslav10@gmail.com>
2026-06-04 20:57:13 +0300

    2 версия

--- db.php
+++ db.php
@@ -3,9 +3,9 @@
 function connectDB()
 {
     return new PDO(
-        'mysql:host=localhost;dbname=restaurant_app;charset=utf8mb4',
-        'root',
-        '',
+        'mysql:host=localhost;dbname=u68592;charset=utf8mb4',
+        'u68592',
+        '1511698',
         [
             PDO::ATTR_ERRMODE =>
                 PDO::ERRMODE_EXCEPTION,
