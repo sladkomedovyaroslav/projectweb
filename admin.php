952af524170a855e143c6cecdae9ca48931b5917
sladkomedovyaroslav <sladkomedov.yaroslav10@gmail.com>
2026-06-04 23:51:51 +0300

    14 версия

--- admin.php
+++ admin.php
@@ -1,12 +1,13 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 require 'db.php';
 
 $pdo = connectDB();
 
+echo '<pre>';
+print_r($_SERVER);
+echo '</pre>';
+exit;
 
 /*
 |--------------------------------------------------------------------------
