fcff98876e5c489ab84ed991a8dadedfac55c324
sladkomedovyaroslav <sladkomedov.yaroslav10@gmail.com>
2026-06-04 21:44:06 +0300

    9 версия

--- admin.php
+++ admin.php
@@ -48,7 +48,7 @@ if (
     !$admin ||
     !password_verify(
         $_SERVER['PHP_AUTH_PW'],
-        $admin['password_hash']
+        $admin['$2y$10$mlqz5/ST7QvHl0oWf3hi.ejfJ1Jo5fNoY0AeGUoBTi99FfvV/UM1K']
     )
 ) {
 
